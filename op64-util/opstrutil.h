0b772c0ff5251e60a77fd18a2367f9cbe151a25d
Simon <homer12345@gmail.com>
2016-01-05 01:48:55 -0800

    Added strlcpy()
    Also disabled Whole Program Optimization because its not worth it

--- op64-util/opstrutil.h
+++ op64-util/opstrutil.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <sstream>
 
+#include "oppreproc.h"
+
 // From https://stackoverflow.com/questions/236129/split-a-string-in-c
 inline std::vector<std::string>& split(const std::string& s, char delim, std::vector<std::string>& elems) {
     std::stringstream ss(s);
@@ -19,4 +21,9 @@ inline std::vector<std::string> split(const std::string& s, char delim) {
     std::vector<std::string> elems;
     split(s, delim, elems);
     return elems;
+}
+
+namespace op
+{
+    OP_API size_t strlcpy(char *dst, const char *src, size_t dsize);
 }
